0ff6c40d5caa8df392fa53d27bc3061c6d39a50b
Paolo Conte <paolo.conte@gmail.com>
2013-07-05 17:21:35 +0200

    Improved comments and code

--- SmoothChartSample/src/org/paoloconte/smoothchart/SmoothLineChart.java
+++ SmoothChartSample/src/org/paoloconte/smoothchart/SmoothLineChart.java
@@ -18,7 +18,7 @@ public class SmoothLineChart extends View {
 	private static final int CHART_COLOR = 0xFF0099CC;
 	private static final int CIRCLE_SIZE = 8;
 	private static final int STROKE_SIZE = 2;	
-	private static final float SMOOTHNESS = 3.0f; // the lower the smoother, but don't go less than 2.0
+	private static final float SMOOTHNESS = 0.3f; // the higher the smoother, but don't go over 0.5
 	
 	private final Paint mPaint;
 	private final Path mPath;
@@ -88,37 +88,35 @@ public class SmoothLineChart extends View {
 		final float right = mValues[mValues.length-1].x;
 		final float dX = (right - left) > 0 ? (right - left) : (2);
 		final float dY =  (mMaxY-mMinY) > 0 ? (mMaxY-mMinY) : (2);
-		
-		
-		mPath.reset();
-
 				
-		List<PointF> points = new ArrayList<PointF>(size);
-		
+		mPath.reset();
+				
+		// calculate point coordinates
+		List<PointF> points = new ArrayList<PointF>(size);		
 		for (PointF point : mValues) {
 			float x = mBorder + (point.x-left)*width/dX;
 			float y = mBorder + height - (point.y-mMinY)*height/dY; 
 			points.add(new PointF(x,y));		
 		}
 
-		
+		// calculate smooth path
 		float lX = 0, lY = 0;
 		mPath.moveTo(points.get(0).x, points.get(0).y);
-		for (int i=0; i<size-1; i++) {	
-			PointF p = points.get(i+1);
+		for (int i=1; i<size; i++) {	
+			PointF p = points.get(i);		// current point
 			
 			// first control point
-			PointF p0 = points.get(i);
-			float d0 = (float) Math.sqrt(Math.pow(p.x - p0.x, 2)+Math.pow(p.y-p0.y, 2));
-			float x1 = Math.min(p0.x + lX*d0, (p0.x+p.x)/2); 	// min is used to avoid going too much right
+			PointF p0 = points.get(i-1);	// previous point
+			float d0 = (float) Math.sqrt(Math.pow(p.x - p0.x, 2)+Math.pow(p.y-p0.y, 2));	// distance between p and p0
+			float x1 = Math.min(p0.x + lX*d0, (p0.x + p.x)/2); 	// min is used to avoid going too much right
 			float y1 = p0.y + lY*d0;
 	
 			// second control point
-			PointF p1 = points.get(i+2 < size ? i+2 : i+1);
-			float d1 = (float) Math.sqrt(Math.pow(p1.x - p0.x, 2)+Math.pow(p1.y-p0.y, 2));
-			lX = (p1.x-p0.x)/d1/SMOOTHNESS;
-			lY = (p1.y-p0.y)/d1/SMOOTHNESS;
-			float x2 = Math.max(p.x - lX*d0, (p0.x+p.x)/2);		 // max is used to avoid going too much left
+			PointF p1 = points.get(i+1 < size ? i+1 : i);	// next point
+			float d1 = (float) Math.sqrt(Math.pow(p1.x - p0.x, 2)+Math.pow(p1.y-p0.y, 2));	// distance between p1 and p0 (length of reference line)
+			lX = (p1.x-p0.x)/d1*SMOOTHNESS;		// (lX,lY) is the slope of the reference line 
+			lY = (p1.y-p0.y)/d1*SMOOTHNESS;
+			float x2 = Math.max(p.x - lX*d0, (p0.x + p.x)/2);		// max is used to avoid going too much left
 			float y2 = p.y - lY*d0;
 
 			// add line
