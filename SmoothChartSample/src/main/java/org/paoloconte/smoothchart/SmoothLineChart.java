080e9ba58ff953f2f8bc4d12525a2a94650dd3fa
paoloconte <paolo.conte@gmail.com>
2017-07-11 16:31:15 +0200

    added support for multiple lines in one chart

--- SmoothChartSample/src/main/java/org/paoloconte/smoothchart/SmoothLineChart.java
+++ SmoothChartSample/src/main/java/org/paoloconte/smoothchart/SmoothLineChart.java
@@ -14,8 +14,17 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class SmoothLineChart extends View {
+
+	public static class Line {
+		final int color;
+		final PointF[] values;
+
+		public Line(int color, PointF[] values) {
+			this.color = color;
+			this.values = values;
+		}
+	}
 	
-	private static final int CHART_COLOR = 0xFF0099CC;
 	private static final int CIRCLE_SIZE = 8;
 	private static final int STROKE_SIZE = 2;	
 	private static final float SMOOTHNESS = 0.3f; // the higher the smoother, but don't go over 0.5
@@ -26,7 +35,7 @@ public class SmoothLineChart extends View {
 	private final float mStrokeSize;
 	private final float mBorder;
 	
-	private PointF[] mValues;
+	private Line[] mLines;
 	private float mMinY;
 	private float mMaxY;
 	
@@ -55,18 +64,19 @@ public class SmoothLineChart extends View {
 		mPath = new Path();
 	}
 	
-	public void setData(PointF[] values) {
-		mValues = values;		
-		
-		if (values != null && values.length > 0) {
-			mMaxY = values[0].y;
-			//mMinY = values[0].y;
-			for (PointF point : values) {
-				final float y = point.y;
-				if (y > mMaxY) 
-					mMaxY = y;
-				/*if (y < mMinY)
-					mMinY = y;*/
+	public void setData(Line[] lines) {
+		mLines = lines;
+
+		if (lines != null) {
+			for (Line line : lines) {
+				if (line.values != null && line.values.length > 0) {
+					mMaxY = line.values[0].y;
+					for (PointF point : line.values) {
+						final float y = point.y;
+						if (y > mMaxY)
+							mMaxY = y;
+					}
+				}
 			}
 		}
 				
@@ -76,71 +86,79 @@ public class SmoothLineChart extends View {
 	public void draw(Canvas canvas) {
 		super.draw(canvas);
 		
-		if (mValues == null || mValues.length == 0)
+		if (mLines == null || mLines.length == 0)
 			return;
 
-		int size = mValues.length;
-		
-		final float height = getMeasuredHeight() - 2*mBorder;	
+		for (Line line : mLines) {
+			drawLine(canvas, line);
+		}
+
+	}
+
+	private void drawLine(Canvas canvas, Line line) {
+
+		int size = line.values.length;
+
+		final float height = getMeasuredHeight() - 2*mBorder;
 		final float width = getMeasuredWidth() - 2*mBorder;
-		
-		final float left = mValues[0].x;
-		final float right = mValues[mValues.length-1].x;
-		final float dX = (right - left) > 0 ? (right - left) : (2);	
+
+		final float left = line.values[0].x;
+		final float right = line.values[line.values.length-1].x;
+		final float dX = (right - left) > 0 ? (right - left) : (2);
 		final float dY = (mMaxY-mMinY) > 0 ? (mMaxY-mMinY) : (2);
-				
+
 		mPath.reset();
-				
+
 		// calculate point coordinates
-		List<PointF> points = new ArrayList<PointF>(size);		
-		for (PointF point : mValues) {
+		List<PointF> points = new ArrayList<PointF>(size);
+		for (PointF point : line.values) {
 			float x = mBorder + (point.x-left)*width/dX;
-			float y = mBorder + height - (point.y-mMinY)*height/dY; 
-			points.add(new PointF(x,y));		
+			float y = mBorder + height - (point.y-mMinY)*height/dY;
+			points.add(new PointF(x,y));
 		}
 
 		// calculate smooth path
 		float lX = 0, lY = 0;
 		mPath.moveTo(points.get(0).x, points.get(0).y);
-		for (int i=1; i<size; i++) {	
+		for (int i=1; i<size; i++) {
 			PointF p = points.get(i);	// current point
-			
+
 			// first control point
 			PointF p0 = points.get(i-1);	// previous point
 			float d0 = (float) Math.sqrt(Math.pow(p.x - p0.x, 2)+Math.pow(p.y-p0.y, 2));	// distance between p and p0
 			float x1 = Math.min(p0.x + lX*d0, (p0.x + p.x)/2); 	// min is used to avoid going too much right
 			float y1 = p0.y + lY*d0;
-	
+
 			// second control point
 			PointF p1 = points.get(i+1 < size ? i+1 : i);	// next point
 			float d1 = (float) Math.sqrt(Math.pow(p1.x - p0.x, 2)+Math.pow(p1.y-p0.y, 2));	// distance between p1 and p0 (length of reference line)
-			lX = (p1.x-p0.x)/d1*SMOOTHNESS;		// (lX,lY) is the slope of the reference line 
+			lX = (p1.x-p0.x)/d1*SMOOTHNESS;		// (lX,lY) is the slope of the reference line
 			lY = (p1.y-p0.y)/d1*SMOOTHNESS;
 			float x2 = Math.max(p.x - lX*d0, (p0.x + p.x)/2);	// max is used to avoid going too much left
 			float y2 = p.y - lY*d0;
 
 			// add line
-			mPath.cubicTo(x1,y1,x2, y2, p.x, p.y);							
+			mPath.cubicTo(x1,y1,x2, y2, p.x, p.y);
 		}
-		
+
 
 		// draw path
-		mPaint.setColor(CHART_COLOR);
+		mPaint.setColor(line.color);
 		mPaint.setStyle(Style.STROKE);
 		canvas.drawPath(mPath, mPaint);
-		
+
 		// draw area
 		if (size > 0) {
 			mPaint.setStyle(Style.FILL);
-			mPaint.setColor((CHART_COLOR & 0xFFFFFF) | 0x10000000);
-			mPath.lineTo(points.get(size-1).x, height+mBorder);	
-			mPath.lineTo(points.get(0).x, height+mBorder);	
+			mPaint.setColor((line.color & 0xFFFFFF) | 0x10000000);
+			mPath.lineTo(points.get(size-1).x, height+mBorder);
+			mPath.lineTo(points.get(0).x, height+mBorder);
 			mPath.close();
 			canvas.drawPath(mPath, mPaint);
 		}
 
 		// draw circles
-		mPaint.setColor(CHART_COLOR);
+		mPaint.setColor(line.color);
 		mPaint.setStyle(Style.FILL_AND_STROKE);
 		for (PointF point : points) {
 			canvas.drawCircle(point.x, point.y, mCircleSize/2, mPaint);
@@ -150,6 +168,6 @@ public class SmoothLineChart extends View {
 		for (PointF point : points) {
 			canvas.drawCircle(point.x, point.y, (mCircleSize-mStrokeSize)/2, mPaint);
 		}
-		
+
 	}
 }
--- SmoothChartSample/src/main/java/org/paoloconte/smoothchart/sample/MainActivity.java
+++ SmoothChartSample/src/main/java/org/paoloconte/smoothchart/sample/MainActivity.java
@@ -15,16 +15,29 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		SmoothLineChart chart = (SmoothLineChart) findViewById(R.id.smoothChart);
-		chart.setData(new PointF[] { 
+
+		SmoothLineChart.Line line1 = new SmoothLineChart.Line(0xFF0099CC, new PointF[] {
 				new PointF(15, 39), // {x, y}
-				new PointF(20, 21), 
-				new PointF(28, 9), 
+				new PointF(20, 21),
+				new PointF(28, 9),
 				new PointF(37, 21),
-				new PointF(40, 25), 
-				new PointF(50, 31), 
-				new PointF(62, 24), 
+				new PointF(40, 25),
+				new PointF(50, 31),
+				new PointF(62, 24),
 				new PointF(80, 28)
-			}); 
+		});
+		SmoothLineChart.Line line2 = new SmoothLineChart.Line(0xFFFF9900, new PointF[] {
+				new PointF(15, 23), // {x, y}
+				new PointF(20, 65),
+				new PointF(28, 34),
+				new PointF(37, 15),
+				new PointF(40, 64),
+				new PointF(50, 34),
+				new PointF(62, 23),
+				new PointF(80, 43)
+		});
+
+		chart.setData(new SmoothLineChart.Line[] { line1, line2 });
 		
 		SmoothLineChartEquallySpaced chartES = (SmoothLineChartEquallySpaced) findViewById(R.id.smoothChartES);
 		chartES.setData(new float[] { 
